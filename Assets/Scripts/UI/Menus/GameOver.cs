068b3507ace5b4bd5561cd0a1eb9b672aecd2f15
Ast <93723066+YaboiAst@users.noreply.github.com>
2025-08-03 10:38:51 -0300

    added game over menu

---
+++ Assets/Scripts/UI/Menus/GameOver.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI indicatorText;
+
+    private void Start()
+    {
+        indicatorText.DOFade(0f, 2f)
+            .SetLoops(-1, LoopType.Yoyo);
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("menu");
+    }
+}
